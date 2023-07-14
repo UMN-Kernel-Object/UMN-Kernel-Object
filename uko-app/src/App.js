f232e49effa175973323073fc3a6cd5b6d0778cf
hanan <hananmabdirahman@gmail.com>
2023-07-14 13:48:58 -0500

    maybe capitial letters is what I was missing

---
+++ uko-app/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+     <h1>UMN-Kernel Object Student Group </h1>
+     <h2>WELCOME TO OUR WEBSITE</h2>
+    </div>
+  );
+}
+
+export default App;
