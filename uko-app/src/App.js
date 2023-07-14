675d58c8394ace917c16831a6cccfbe53d768314
hanan <hananmabdirahman@gmail.com>
2023-07-14 14:49:26 -0500

    is it really a commit if you commit

--- uko-app/src/App.js
+++ uko-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
      <h1>UMN-Kernel Object Student Group </h1>
      <h2>WELCOME TO OUR WEBSITE</h2>
+     <h3>WE ARE GLAD TO SEE YOU HERE</h3>
     </div>
   );
 }
