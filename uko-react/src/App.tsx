e8cfa85d696e1338247255c22cd494de3b0ba7ef
hanan <hananmabdirahman@gmail.com>
2023-07-20 13:11:39 -0500

    navbar with link to documentation

---
+++ uko-react/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavbarComponent from './components/NavbarComponent';
+
+function App() {
+  const links = [
+    {text: 'About', url:'/'},
+    {text: 'Documentation', url: '/docs'}
+  ];
+  return (
+    <NavbarComponent title = "UMN-Kernel Object" links = {links}/>
+  );
+}
+
+
+
+
+
+
+export default App;
