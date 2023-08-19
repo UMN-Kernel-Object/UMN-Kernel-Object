5fbe43571ecd8fbd3b228288cffdd4facbd60902
hanan <hananmabdirahman@gmail.com>
2023-08-19 13:35:31 -0500

    Updates

---
+++ precache-manifest.1521d2e34e0038df5f40796c1bee3a9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e3d7285ebf16f824577",
+    "url": "/static/js/main.3e0473ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e02e77a748cc9d815ba",
+    "url": "/static/css/2.25b867a2.chunk.css"
+  },
+  {
+    "revision": "0e02e77a748cc9d815ba",
+    "url": "/static/js/2.8cc86784.chunk.js"
+  },
+  {
+    "revision": "f535f4f10d7031458106da44750fb2ef",
+    "url": "/index.html"
+  }
+];
